a0e3c1c77024150c7d55289ac6a9cef70bafc967
taggartjames <150398666+taggartjames@users.noreply.github.com>
2023-11-10 06:23:18 -0500

    Create server.js
    set up your Express.js server

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const fetch = require('node-fetch');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get('/proxy', async (req, res) => {
+  try {
+    const url = req.query.url;
+    const response = await fetch(url);
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
